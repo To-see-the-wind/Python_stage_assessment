891091cea1d93a3bdc7b75fa89eecc64157384b3
To-see-the-wind <Tony21612823@outlook.com>
2026-09-25 20:47:48 +0800

    第三部分:NumPy 按列归一化、均值标准差、异常值筛选

--- 3.py
+++ 3.py
@@ -2,13 +2,9 @@ import pandas as pd
 
 train=pd.read_csv('train_u6lujuX_CVtuZ9i.csv')
 train.LoanAmount=train.LoanAmount.fillna(train.LoanAmount.mean())
-df=train.loc[:,['ApplicantIncome','CoapplicantIncome','LoanAmount']]
-df['ApplicantIncome']=(df['ApplicantIncome']-df['ApplicantIncome'].min())/(df['ApplicantIncome'].max()-df['ApplicantIncome'].min())
-df['CoapplicantIncome']=(df['CoapplicantIncome']-df['CoapplicantIncome'].min())/(df['CoapplicantIncome'].max()-df['CoapplicantIncome'].min())
-df['LoanAmount']=(df['LoanAmount']-df['LoanAmount'].min())/(df['LoanAmount'].max()-df['LoanAmount'].min())
-mean0=df.mean(axis=0)#axis=0表示一列一列算，每一列计算一个均值
-mean1=df.mean(axis=1)
-std0=df.std(axis=0)
-std1=df.std(axis=1)
-print((df['ApplicantIncome']>mean0['ApplicantIncome']+2*std0['ApplicantIncome']).sum())
+X=train[['ApplicantIncome','CoapplicantIncome','LoanAmount']].to_numpy()
+X_norm=(X-X.min(axis=0))/(X.max(axis=0)-X.min(axis=0))
+mean0=X_norm.mean(axis=0)#axis=0表示一列一列算，每一列计算一个均值
+std0=X_norm.std(axis=0)
+print((X_norm[:,0]>mean0[0]+2*std0[0]).sum())
 #回答5：shape是（614，3），能乘，结果是（614，1）
